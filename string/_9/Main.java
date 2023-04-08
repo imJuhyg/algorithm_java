48cd9cf58d1d991bf89fdce7e9e47d5193c79c54
imJuhyg <limjuhyg@gmail.com>
2023-04-08 17:22:04 +0900

    숫자만 추출

--- string/_9/Main.java
+++ string/_9/Main.java
@@ -3,35 +3,23 @@ package string._9;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 // 숫자만 추출
 public class Main {
-    public String solution(String input) {
-        char[] arr = input.toCharArray();
-        ArrayList<Integer> list = new ArrayList<>();
-        for(char c : arr) {
-            if(c >= '0' && c <= '9') {
-                list.add(c - '0');
-            }
+    public int solution(String input) {
+        StringBuilder answer = new StringBuilder();
+        for(char c : input.toCharArray()) {
+            if(Character.isDigit(c)) answer.append(c);
         }
 
-        StringBuilder sb = new StringBuilder();
-        boolean flag = false;
-        for(int i=0; i<list.size(); i++) {
-            if(list.get(i) == 0 && !flag) continue;
-            flag = true;
-            sb.append(list.get(i));
-        }
-
-        return sb.toString();
+        return Integer.parseInt(answer.toString());
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        String answer = main.solution(input);
+        int answer = main.solution(input);
         System.out.println(answer);
     }
 }
