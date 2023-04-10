3ccace4ac7c05a6e5138731ba9565eeff03efb24
imJuhyg <limjuhyg@gmail.com>
2023-04-10 17:41:56 +0900

    학급 회장(해쉬)

---
+++ hash/_1/Main.java
@@ -0,0 +1,31 @@
+package hash._1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+// 학급 회장(해쉬)
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : input.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        char answer = 'a';
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > max) {
+                max = entry.getValue();
+                answer = entry.getKey();
+            }
+        }
+        System.out.println(answer);
+    }
+}
